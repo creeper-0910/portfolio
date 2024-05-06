c9ad064db26a2c4eb944bb30e511ee2900949f14
creeper-0910 <56744841+creeper-0910@users.noreply.github.com>
2024-05-06 13:15:50 +0900

    イラスト表示実装

--- src/components/IllustView.vue
+++ src/components/IllustView.vue
@@ -2,19 +2,38 @@
     <div>
     <v-container>
       <v-row no-gutters align="start" class="custom-font">
+      <v-col cols="12" class="d-flex flex-row justify-center title-margin">
+          <font size="30">イラスト</font>
+      </v-col>
       <v-col class="d-flex flex-row justify-center title-margin">
-          <font size="30">今までのイラスト</font>
+        <font size="2">※並びは仕様上ランダムです</font>
       </v-col>
     </v-row>
     </v-container>
     <v-divider class="divider-margin"></v-divider>
-
-
+    <v-container>
+      <v-row>
+          <v-col cols="6" v-for="illusts in illust_url" :key="illusts.key">
+            <v-img :src="illusts"/>
+          </v-col>
+    </v-row>
+  </v-container>
   </div>
 </template>
 
 <script setup lang="ts">
-//
+import { onMounted, ref } from 'vue';
+const illust_url = ref()
+onMounted(async () => {
+  try {
+        const response = await fetch('https://raw.githubusercontent.com/creeper-0910/illust/main/data.txt');
+        const text = await response.text();
+        const lines = text.split('\n');
+        illust_url.value = lines.map(item => "https://raw.githubusercontent.com/creeper-0910/illust/main" + item);
+      } catch (error) {
+        console.error('テキストファイルの取得やパース中にエラーが発生しました:', error);
+      }
+});
 </script>
 
 <style>
